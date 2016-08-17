7177aacd98ae93d356bd83d10aac22728f59b1a8
bushan nagendra <bushan1891@gmail.com>
2016-08-17 14:15:51 -0700

    implimented props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
-import videoList from './components/video_list';
+import VideoList from './components/video_list';
 // get it from cosole.google
 const API_KEY = 'AIzaSyAK0sIaPpQr773qfrxx9l6S5kVFhGCFD2Q';
 // this is the componet class  //  React.CreateClass
@@ -10,23 +10,41 @@ const API_KEY = 'AIzaSyAK0sIaPpQr773qfrxx9l6S5kVFhGCFD2Q';
      return <h1 > Hey there! < /h1>
  } */
 // using ES 6 fat arrow for function //  this will have differnt this useage
-class App extends Component {
-	constructor() {
-		super( props );
-		// setting up the state
-		this.state = {
-			videos: []
-		};
-		// making call to youtube for the data end point
-		YTSearch( {	key: API_KEY,term :'surfboards'}, ( video ) => {this.setState( {videos: video} )} );
-	}
 
-	render() {
+
+
+class  App extends Component {
+	constructor(props) {
+
+							super(props);
+
+							YTSearch({key:API_KEY,term:'surfboards'},(data) => {
+								this.setState({videos:data});
+							})
+
+							this.state={videos:[]};
+
+						}
+
+		render(){
+			return (
+						<div>
+							< SearchBar />
+							< VideoList videos={this.state.videos} />
+						</div>
+					);
+			 }
+
+}
+
+
+// this is a functional
+/*const App = ()=> {
 		// we are passing props videoList
 		return ( < div >
               < SearchBar / >
             < / div > );
-	}
-}
+
+}*/
 // <App /> is componenet instance // React.createElement
 ReactDOM.render( < App / > , document.querySelector( '.container' ) );
